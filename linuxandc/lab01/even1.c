7d12410df3f13a5ea3690030df692579286b2113
Luciomormile <135097369+Luciomormile@users.noreply.github.com>
2023-06-12 14:57:56 +0200

    Add files via upload

---
+++ linuxandc/lab01/even1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	int n = 12;
+
+        if(n%2==0) {
+	       
+	printf("%d e' pari\n",n	);
+	} else {
+		printf("%d e' dispari \n",n);
+	}
+	return 0;
+	
+}	
+
+
+
+
+
+
+
+ 
+
+
---
+++ linuxandc/lab01/numbers1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int n = 5;
+	int i;
+        i = 1;
+	while (i<=n) {
+		printf("%d...", i);
+		i++;
+	}
+	printf("\n");
+	return 0;
+	
+	
+}	
+
+
+
+
+
+
+
+ 
+
+
